4968094d7f19d970194deaa5e66b6ad420155554
Росица Бъндева <rossy@compass-bg.net>
2016-12-30 20:56:23 +0200

    Add login and register components and add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './routing/app-routing.module'
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
 import { CarsListComponent } from './car/cars-list/cars-list/cars-list.component';
 
 import { CarService } from './services/car.service';
@@ -15,13 +19,16 @@ import { CarItemComponent } from './car/car-item/car-item/car-item.component';
   declarations: [
     AppComponent,
     NavbarComponent,
+    LoginComponent,
+    RegisterComponent, 
     CarsListComponent,
     CarItemComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [CarService],
   bootstrap: [AppComponent]
