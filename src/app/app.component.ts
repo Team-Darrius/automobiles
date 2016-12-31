96bd173379937ea92092a612e36c000eba90a38f
VenelinGP <venelin.petkov@netcoms.eu>
2016-12-31 13:25:39 +0200

    Added Font Awesome and logout button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { AngularFire } from 'angularfire2'
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -9,18 +8,14 @@ import { Router } from '@angular/router';
 })
 
 export class AppComponent {
-  constructor(private af: AngularFire, private router: Router){}
 
-  logout() {
-    this.af.auth.logout();
-    this.router.navigate(['/'])
-  }
 
   title = 'app works!';
 }
 
+
 @Component({
-  templateUrl: 'app/page.not.found.html'
+  templateUrl: './page.not.found.html'
 })
 
 export class PageNotFoundComponent {}
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire } from 'angularfire2'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent{
 
-  constructor() { }
+  constructor(private af: AngularFire, private router: Router){}
 
-  ngOnInit() {
+  logout() {
+    this.af.auth.logout();
+    this.router.navigate(['/'])
   }
 
 }
