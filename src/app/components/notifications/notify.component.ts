c8a982f80fdcaca7c98390d0843f402c7c435fea
VenelinGP <venelin.petkov@netcoms.eu>
2017-01-03 08:56:22 +0200

    Added notify animation.

--- src/app/components/notifications/notify.component.ts
+++ src/app/components/notifications/notify.component.ts
@@ -6,34 +6,42 @@ import { NotificationService } from '../../services/index';
     selector: 'app-notification',
     animations: [
         trigger('visibilityChanged', [
-            state('true' , style({ opacity: 1, transform: 'scale(1.0)' })),
-            state('false', style({ opacity: 0, transform: 'scale(0.0)'  })),
-            transition('1 => 0', animate('300ms')),
-            transition('0 => 1', animate('900ms'))
+            // state('true' , style({ opacity: 1, transform: 'scale(1.0)' })),
+            // state('false', style({ opacity: 0, transform: 'scale(0.0)'  })),
+            // transition('1 => 0', animate('300ms')),
+            // transition('0 => 1', animate('900ms'))
+            state('in', style({transform: 'translateX(0)'})),
+            transition('void => *', [
+                style({transform: 'translateX(-100%)'}),
+            animate(100)
+            ]),
+            transition('* => void', [
+            animate(100, style({transform: 'translateX(100%)'}))
+            ])
         ])
     ],
     templateUrl: './notify.component.html',
     styleUrls: ['./notify.component.css']
 })
 
-export class NotificationComponent implements OnInit, OnChanges {
+export class NotificationComponent implements OnInit {
     message: string;
     @Input() isVisible : boolean = true;
      visibility = 'shown';
 
     constructor(private notificationService: NotificationService) { }
 
-    ngOnChanges() {
-        this.visibility = this.isVisible ? 'shown' : 'hidden';
-    }
+    // ngOnChanges() {
+    //     this.visibility = this.isVisible ? 'shown' : 'hidden';
+    // }
 
     ngOnInit() {
-        this.visibility = 'shown';
+        // this.visibility = 'shown';
         this.notificationService.getMessage().subscribe(message => { this.message = message; });
     }
 
-    closeAlert(element: HTMLElement) {
-        element.classList.remove('in');
-        setTimeout(() => this.message = null, 150);
-    }
+    // closeAlert(element: HTMLElement) {
+    //     element.classList.remove('in');
+    //     setTimeout(() => this.message = null, 150);
+    // }
 }
